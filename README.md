99026432fc4695bb3efc2cc0c361f8d2941146c6
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-11-26 00:27:11 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_ivv
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_ivv/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_ivv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
